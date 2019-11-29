61b13d21cbd8f94b112968521198de03e355be74
Alex Oliveira <alexoliverasince1989@hotmail.com>
2019-11-29 20:18:25 -0300

    adicionando jpa e spring data

--- src/main/java/com/example/uze/model/Vendedor.java
+++ src/main/java/com/example/uze/model/Vendedor.java
@@ -2,11 +2,18 @@ package com.example.uze.model;
 
 import java.io.Serializable;
 
-//@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Vendedor implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String nome;
