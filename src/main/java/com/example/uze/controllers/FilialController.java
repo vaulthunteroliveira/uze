a6bdf74c04f7b7591549f9a153f1753ebd1b1fa0
Alex Oliveira <alexnosaint@gmail.com>
2019-12-01 15:56:32 -0300

    resolvendo erro tela de listagem

--- src/main/java/com/example/uze/controllers/FilialController.java
+++ src/main/java/com/example/uze/controllers/FilialController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,7 +26,7 @@ public class FilialController {
 		return "/filial/cadastro";
 	}
 
-	@GetMapping("/listar")
+	@GetMapping("listar")
 	private String listar(ModelMap modelMap) {
 		modelMap.addAttribute("filiais", service.listar());
 		return "/filial/lista";
